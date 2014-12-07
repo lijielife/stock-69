99ad449924ed19a6f7f1ba8c61e23ca93694b2e4
upton <updn@live.com>
2014-12-07 21:10:39 +0800

    add detail page

---
+++ detail.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on 2014年12月7日
+
+@author: upton
+'''
+
+class detail:
+    '''
+    classdocs
+    '''
+    def GET(self):
+        return "detail"
+        
--- index.py
+++ index.py
@@ -3,7 +3,10 @@
 
 import web
 
-urls = ("/.*", "hello")
+urls = (
+        "/.*", "hello"
+        "/detail", "detail"
+)
 app = web.application(urls, globals())
 
 class hello:
@@ -11,5 +14,5 @@ class hello:
         return 'Hello, world!'
 
 if __name__ == "__main__":
-    web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
+    web.wsgi.runwsgi = lambda func, addr = None: web.wsgi.runfcgi(func, addr)
     app.run()
